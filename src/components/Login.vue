9a1e6ece30fd3d49da54dbe7955094242e3822bd
HUAWEI <MateBook13@outlook.com>
2020-09-16 01:14:38 +0800

    登录组件表单验证功能

---
+++ src/components/Login.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+
+      <!-- 表单区域 -->
+      <!-- 第12、13步，数据绑定和表单验证规则 -->
+      <el-form label-width="0px" class="login_form" :model="loginForm" :rules="loginFormRules">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input prefix-icon="iconfont icon-account-fill" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input prefix-icon="iconfont icon-password" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="login_btn">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 表单的数据绑定对象
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      // 表单的验证规则对象
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 10, message: '长度在 6 到 10 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+  .login_form {
+    padding: 0 15px;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    box-sizing: border-box;
+    .login_btn {
+      display: flex;
+      justify-content: flex-end;
+    }
+  }
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    .avatar_box {
+      width: 80px;
+      height: 80px;
+      border: 1px solid #eeeeee;
+      border-radius: 50%;
+      box-shadow: 0 0 10px #dddddd;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      margin-top: 20px;
+      img {
+        height: 100%;
+        width: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+}
+</style>
